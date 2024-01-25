cf3d55030fcb2107b43e8ec4e5ee7dc457948522
Olena <pysarenkoelena@gmail.com>
2024-01-25 23:22:11 +0200

    Add project files.

---
+++ CoolChat.Client/Program.cs
@@ -0,0 +1,121 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CoolChat.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using TcpClient tcpClient = new TcpClient(AddressFamily.InterNetwork);
+
+            try
+            {
+                UdpClient scanClient = new UdpClient(AddressFamily.InterNetwork);
+                scanClient.Client.ReceiveTimeout = 2000;
+
+                UdpReceiveResult result = default;
+                string message = string.Empty;
+
+                try
+                {
+                    for (int i = 1; i <= 5; i++)
+                    {
+                        Console.WriteLine("Scan network for the chat server. Try " + i + ".");
+                        await scanClient.SendAsync(Encoding.UTF8.GetBytes("SCAN BY COOL CHAT SERVER"), new IPEndPoint(IPAddress.Broadcast, 7701));
+                        try
+                        {
+                            IPEndPoint? remoteEndPoint = null;
+                            //result = await scanClient.ReceiveAsync();
+                            var data = scanClient.Receive(ref remoteEndPoint);
+                            result = new UdpReceiveResult(data, remoteEndPoint);
+                            message = Encoding.UTF8.GetString(result.Buffer);
+
+                            Console.WriteLine($"Scan receive message [{message}] from {remoteEndPoint}");
+
+                            if (!message.StartsWith("YES"))
+                            {
+                                Console.WriteLine("Servers not found!");
+                                return;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Servers not found!");
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Servers not found!");
+                    return;
+                }
+
+                var port = int.Parse(message.Split(':')[1]);
+                var ip = result.RemoteEndPoint.Address;
+                Console.WriteLine($"Server found at {ip}:{port}");
+
+                tcpClient.Connect(ip, port/*"192.168.1.253", 7700*/);
+
+                Console.WriteLine("Connection established!");
+
+                using var stream = tcpClient.GetStream();
+
+                var readTask = Read(stream);
+                var writeTask = Write(stream);
+
+                Task.WaitAll(readTask, writeTask);
+            }
+            catch
+            {
+                Console.WriteLine("Client was disconnected");
+            }
+        }
+
+        static Task Read(NetworkStream stream)
+        {
+            return Task.Run(async () =>
+            {
+                var endPoint = stream.Socket.LocalEndPoint;
+                var reader = new StreamReader(stream);
+                while (true)
+                {
+                    var msg = await reader.ReadLineAsync();
+                    if (!string.IsNullOrEmpty(msg))
+                    {
+                        var splittedMsg = msg.Split("@");
+                        if (splittedMsg[0] == endPoint?.ToString())
+                        {
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(msg);
+                        }
+                    }
+                }
+            });
+        }
+
+        static Task Write(Stream stream)
+        {
+            return Task.Run(async () =>
+            {
+                var writer = new StreamWriter(stream);
+                while (true)
+                {
+                    //Console.Write("Enter the text: ");
+                    var text = Console.ReadLine();
+                    await writer.WriteLineAsync(text);
+                    await writer.FlushAsync();
+                }
+            });
+        }
+    }
+}
---
+++ CoolWebServer/Program.cs
@@ -0,0 +1,154 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace CoolWebServer
+{
+    class Request
+    {
+        public string Method { get; set; }
+        public string Path { get; set; }
+        public string Protocol { get; set; }
+
+        public Dictionary<string, string> Headers { get; } = new Dictionary<string, string>();
+
+        public static async Task<Request> Parse(StreamReader reader)
+        {
+            var line = await reader.ReadLineAsync();
+            var segments = line.Split(" ");
+            var request = new Request() { Method = segments[0], Path = segments[1], Protocol = segments[2] };
+
+            line = await reader.ReadLineAsync();
+            while(!string.IsNullOrEmpty(line))
+            {
+                segments = line.Split(":");
+                request.Headers[segments[0]] = segments[1];
+
+                line = await reader.ReadLineAsync();
+            }
+
+            return request;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{Method} {Path} {Protocol}");
+
+            foreach( var kvp in Headers)
+            {
+                sb.AppendLine($"{kvp.Key} : {kvp.Value}");
+            }
+            return sb.ToString();
+        }
+    }
+
+    class Response
+    {
+
+        public string Protocol { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusDesc { get; set; }
+
+        public string Body { get; set; }
+
+        public Dictionary<string, string> Headers { get; } = new Dictionary<string, string>();
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{Protocol} {StatusCode} {StatusDesc}");
+
+            foreach (var kvp in Headers)
+            {
+                sb.AppendLine($"{kvp.Key} : {kvp.Value}");
+            }
+
+            sb.AppendLine();
+            sb.AppendLine(Body);
+
+            return sb.ToString();
+        }
+    }
+
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            TcpListener server = new TcpListener(System.Net.IPAddress.Any, 8081);
+            server.Start();
+
+            Console.WriteLine($"Server started on: {server.Server.LocalEndPoint}");
+            try
+            {
+                while (true)
+                {
+                    using var client = await server.AcceptTcpClientAsync();
+                    using var stream = client.GetStream();
+                    using var reader = new StreamReader(stream);
+                    using var writer = new StreamWriter(stream);
+
+                    var request = await Request.Parse(reader);
+                    Console.WriteLine($"client: {client.Client.RemoteEndPoint}");
+                    Console.WriteLine(request.ToString());
+                    Console.WriteLine("--------------------------------------------");
+
+                    var response = await GetResponse(request, writer);
+                    Console.WriteLine("Response send to client:");
+                    Console.WriteLine(response.ToString());
+                    Console.WriteLine("============================================");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                server.Stop();
+            }
+        }
+
+        private static string? GetName(Request request)
+        {
+            var splitted = request.Path.Split('?');
+            splitted = splitted[1].Split('&');
+
+            for (int i = 0; i < splitted.Length; i++)
+            {
+                var pair = splitted[i];
+                var splittedPair = pair.Split('=');
+                if (splittedPair[0].ToLower() == "name")
+                {
+                    return splittedPair[1];
+                }
+            }
+
+            return null;
+        }
+
+        private static async Task<Response> GetResponse(Request request, StreamWriter writer)
+        {
+            var name = GetName(request);
+            var body = name == null
+                ? "<html> <body><h1>Hello, World!</h1></body></html>"
+                : $"<html> <body><h1>Hello, {name}!</h1></body></html>";
+
+            var response = new Response()
+            {
+                Protocol = "HTTP/1.1",
+                StatusCode = 200,
+                StatusDesc = "OK",
+                Headers = {
+                    { "Content-Length", body.Length.ToString() },
+                    { "Content-Type", "text/html" },
+                    { "Connection", "Closed" },
+                    { "Server", "MyCoolWebServer/1.0.0 (Win32)" }
+                },
+                Body = body
+            };
+
+            await writer.WriteLineAsync(response.ToString());
+            await writer.FlushAsync();
+            return response;
+        }
+    }
+}
